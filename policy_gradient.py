46c84ec9da80b8cfb431d495a58607637ecbf73b
Niranjan <kniranjankumar.eee@gmail.com>
2018-03-28 13:27:05 -0400

    ddpg for cartpole

--- policy_gradient.py
+++ policy_gradient.py
@@ -5,10 +5,11 @@ from replay_buffer import Memory
 from matplotlib import pyplot as plt
 
 class Value:
-    def __init__(self, env):
+    def __init__(self, env,scope):
+        self.scope = scope
         self.env = env
         self.obs_space = self.env.observation_space.shape[0]
-        with tf.variable_scope("value_fn", reuse=tf.AUTO_REUSE):
+        with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
             self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-2)
             self.obs = tf.placeholder(tf.float32, shape=[None,self.obs_space], name="observation")
             self.action = tf.placeholder(tf.float32, shape=[None,1], name="action")
@@ -19,7 +20,7 @@ class Value:
 
     def value_model(self, obs, action):
         hidden = 64
-        with tf.variable_scope("value_fn", reuse=tf.AUTO_REUSE):
+        with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
             # obs = tf.placeholder(tf.float32,shape=self.env.observation_space.shape,name="observation")
             # target = tf.placeholder(tf.float32,shape=[1],name="target_action")
             dense1 = tf.layers.dense(inputs=obs, units=hidden, activation=tf.nn.relu)
@@ -27,9 +28,19 @@ class Value:
             dense2 = tf.layers.dense(inputs=dense1, units=hidden, activation=tf.nn.relu)
             value = tf.layers.dense(inputs=dense2, units=1, activation=tf.nn.tanh)
             return value
+    def get_trainable_parameters(self):
+        with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
+            policy_parameters = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=self.scope)
+            return policy_parameters
+
+    def set_trainable_parameters(self,parameters,tau):
+        this_policy_parameters = self.get_trainable_parameters()
+        self.soft_assign = [tf.assign(v_t, v*tau+v_t*(tau-1)) for v_t, v in zip(this_policy_parameters,parameters)]
+        sess = tf.get_default_session()
+        sess.run(self.soft_assign)
 
     def get_q_gradient(self,action,obs):
-        with tf.variable_scope("value_fn", reuse=tf.AUTO_REUSE):
+        with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
             obs = obs.reshape(-1, self.obs_space)
             action = action.reshape(-1, 1)
             sess = tf.get_default_session()
@@ -37,38 +48,41 @@ class Value:
             return sess.run([grad],feed_dict={self.action:action,self.obs:obs})
 
     def predict(self, obs, action):
-        obs = obs.reshape(-1, self.obs_space)
-        action = action.reshape(-1,1)
-        sess = tf.get_default_session()
-        return sess.run([self.value], feed_dict={self.obs: obs, self.action: action})
-
+        with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
+            obs = obs.reshape(-1, self.obs_space)
+            action = action.reshape(-1,1)
+            sess = tf.get_default_session()
+            value = sess.run([self.value], feed_dict={self.obs: obs, self.action: action})
+            # tf.summary.scalar('value', value)
+            return value
     def update_value(self, obs, action, target):
-        with tf.variable_scope("value_fn", reuse=tf.AUTO_REUSE):
+        with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
             sess = tf.get_default_session()
             feed_dict = {self.obs: obs, self.target: target, self.action:action}
-            loss, summary = sess.run([self.loss,self.optimize_value], feed_dict)
-            return summary
+            loss, _ = sess.run([self.loss,self.optimize_value], feed_dict)
 
 class Policy:
-    def __init__(self, env):
-        self.env = env
-        self.action_space = 1
-        self.obs_space = self.env.observation_space.shape[0]
-        self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-2)
-        self.obs = tf.placeholder(tf.float32, shape=[None,self.obs_space], name="observation")
-        self.target = tf.placeholder(tf.float32, shape=[None,self.action_space], name="target_action")
-        self.action = self.policy_model(self.obs)
+    def __init__(self, env,scope):
+        self.scope = scope
+        with tf.variable_scope(scope, reuse = tf.AUTO_REUSE):
+            self.env = env
+            self.action_space = 1
+            self.obs_space = self.env.observation_space.shape[0]
+            self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-2)
+            self.obs = tf.placeholder(tf.float32, shape=[None, self.obs_space], name="observation")
+            self.target = tf.placeholder(tf.float32, shape=[None, self.action_space], name="target_action")
+            self.action = self.policy_model(self.obs)
 
     def noise(self, mu=None, sigma=None):
         if mu is None:
-            mu = np.zeros(self.env.action_space.shape)
+            mu = np.zeros([1,self.action_space])
         if sigma is None:
             sigma = 0.01
-        return np.random.normal(mu, sigma * np.ones(self.action_space))
+        return np.random.normal(mu, sigma * np.ones([1,self.action_space]))
 
     def policy_model(self, obs):
         hidden = 64
-        with tf.variable_scope("policy", reuse=tf.AUTO_REUSE):
+        with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
             # obs = tf.placeholder(tf.float32,shape=self.env.observation_space.shape,name="observation")
             # target = tf.placeholder(tf.float32,shape=[1],name="target_action")
             dense1 = tf.layers.dense(inputs=obs, units=hidden, activation=tf.nn.relu)
@@ -82,17 +96,28 @@ class Policy:
         return sess.run([self.action], feed_dict={self.obs: obs}) + self.noise()
 
     def update_policy(self, obs, target):
-        with tf.variable_scope("policy", reuse=tf.AUTO_REUSE):
+        with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
             sess = tf.get_default_session()
             feed_dict = {self.obs: obs, self.target: target}
             loss, summary = sess.run([self.optimize_policy], feed_dict)
             return summary
 
+    def get_trainable_parameters(self):
+        with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
+            policy_parameters = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=self.scope)
+            return policy_parameters
+
+    def set_trainable_parameters(self,parameters,tau):
+        this_policy_parameters = self.get_trainable_parameters()
+        self.soft_assign = [tf.assign(p_t, p*tau+p_t*(tau-1)) for p_t, p in zip(this_policy_parameters,parameters)]
+        sess = tf.get_default_session()
+        sess.run(self.soft_assign)
+
     def optimize_policy(self,q_grad,obs):
-        with tf.variable_scope("policy", reuse=tf.AUTO_REUSE):
+        with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
             obs = obs.reshape(-1, self.obs_space)
             Q_gradient= tf.placeholder(tf.float32,shape=[None,1],name="Q_gradient")
-            policy_parameters = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope="policy")
+            policy_parameters = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope=self.scope)
             policy_grad = tf.gradients(self.action,policy_parameters,Q_gradient)
             # print(policy_parameters)
             # loss = tf.reduce_mean(policy_grad)
@@ -102,17 +127,19 @@ class Policy:
             # tf.summary.scalar('loss', tf.reduce_mean(loss))
             # merged = tf.summary.merge_all()
 
-gamma = 0.1
+gamma = 0.99
 episodes = 100
-batch_size = 10
+batch_size = 128
 max_time_steps = 200
 episode_reward = 0
 reward_history = []
 env = gym.make('CartPole-v0')
 obs_old = env.reset()
 memory = Memory(limit=int(1e6), action_shape=env.action_space.shape, observation_shape=env.observation_space.shape)
-agent_policy = Policy(env)
-agent_critic = Value(env)
+agent_policy = Policy(env,"policy")
+agent_critic = Value(env,"value")
+agent_policy_t = Policy(env,"policy_t")
+agent_critic_t = Value(env,"value_t")
 #initial rollouts to gather date
 for i in range(100):
     action = env.action_space.sample()
@@ -126,14 +153,24 @@ for i in range(100):
         episode_reward = 0
         env.reset()
 episode_reward = 0
+time = 200
+tf.summary.scalar("episode_time_steps", time)
+tf.summary.scalar("episode_reward", episode_reward)
+merged = tf.summary.merge_all()
 with tf.Session() as sess:
+    from datetime import datetime
+    now = datetime.now()
+    train_writer = tf.summary.FileWriter('./train/'+now.strftime("%Y%m%d-%H%M%S")+'/', sess.graph)
+    # train_writer = tf.summary.FileWriter('.' + '/train', sess.graph)
     sess.run(tf.global_variables_initializer())
+    agent_critic_t.set_trainable_parameters(agent_critic.get_trainable_parameters(), 0)
+    agent_policy_t.set_trainable_parameters(agent_policy.get_trainable_parameters(), 0)
     for i in range(episodes):
         print('running episode:',i)
         t = 0
         done = 0
         while not done and t < max_time_steps:
-            action = agent_policy.predict(obs_old)
+            action = agent_policy.predict(obs_old) #+ agent_policy.noise(0,0.1)
             if action>0.5:
                 action = 1
             else:
@@ -144,21 +181,27 @@ with tf.Session() as sess:
             episode_reward += rew
             memory.append(obs_old, action, rew, obs, done)
             if done or t == max_time_steps-1:
+                time = t
                 reward_history.append(episode_reward)
-                episode_reward = 0
                 env.reset()
             obs_old = obs
             t += 1
             batch = memory.sample(batch_size)
             obs_batch = batch['obs0']
-            action_batch_predict = agent_policy.predict(obs_batch)[0]
-            value_batch = agent_critic.predict(obs_batch,action_batch_predict)[0]
+            action_batch_predict = agent_policy_t.predict(obs_batch)[0]
+
+            value_batch = agent_critic_t.predict(obs_batch,action_batch_predict)[0]
             y = np.array(batch['rewards']) + gamma* np.array(value_batch)#.reshape(-1,batch_size)
             agent_critic.update_value(obs_batch,action_batch_predict,y)
             q_grad = np.array(agent_critic.get_q_gradient(action_batch_predict, obs_batch)).reshape(-1,1)
             agent_policy.optimize_policy(q_grad,obs_batch)
+            agent_critic_t.set_trainable_parameters(agent_critic.get_trainable_parameters(), 0.001)
+            agent_policy_t.set_trainable_parameters(agent_policy.get_trainable_parameters(), 0.001)
+        # print(episode_reward)
+        summary = sess.run(merged)
+        train_writer.add_summary(summary, i)
+        time = 200
+        episode_reward = 0
 
-            # print()
-
-print(reward_history)
-plt.plot(reward_history)
+# print(reward_history)
+# plt.plot(reward_history)
